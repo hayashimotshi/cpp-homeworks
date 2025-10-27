9897222be5e7a990c7551335e807e5141e9265bb
hayashimotshi <motshi@proton.me>
2025-10-27 21:13:14 +0400

    HomeworkMy-WC (#2)
    * HomeworkMy-WC
    
    * исправлено
    
    * fix: исправил замечания из ревью
    
    * minor fix (final)
    
    ---------
    
    Co-authored-by: Denis Shamonin <motshi@noiseofnewyorkstrett.local>
    Co-authored-by: Denis Shamonin <motshi@lapfukingtop.local>

--- task1/main.c
+++ task1/main.c
@@ -1,3 +1,32 @@
-int main(void) {
-    // Тут могло быть ваше решение.
+#include <stdio.h>
+
+void wc(FILE *file) {
+    unsigned int charcounter = 0, wordcounter = 0, strcounter = 0;
+    char buff;
+    while ((buff = fgetc(file)) != EOF) {
+        charcounter++;
+        if (buff == ' ' || buff == '\t' || buff == '\n' || buff == '\r' ||
+            buff == '\v') {
+            wordcounter++;
+        }
+        if (buff == '\n') {
+            strcounter++;
+        }
+    }
+    printf("%d\n%d\n%d\n", strcounter, wordcounter, charcounter);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc > 1) {
+        FILE *file = fopen(argv[1], "r");
+        if (!file) {
+            fprintf(stderr, "Error: Invalid file name\n");
+            return 1;
+        }
+        wc(file);
+        fclose(file);
+    }
+    if (argc == 1) {
+        wc(stdin);
+    }
 }
